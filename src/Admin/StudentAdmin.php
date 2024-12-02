be86d4eef7969601d21717fd478a736a43c01afd
Jonika <katyadyduch@gmail.com>
2024-12-02 23:13:53 +0200

    student admin show

--- src/Admin/StudentAdmin.php
+++ src/Admin/StudentAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\AdminType;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class StudentAdmin extends AbstractAdmin
@@ -44,6 +45,64 @@ final class StudentAdmin extends AbstractAdmin
             ->add('associatedUser.birthday', null, [
                 'label' => 'Birthday',
             ])
+            ->add('_action', 'actions', [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                ],
+            ])
         ;
+    }
+
+    public function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->with('Student Details')
+            ->add('associatedUser.avatarPath', null, [
+                'template' => 'admin/image_preview.html.twig',
+                'label' => 'Avatar',
+                'path' => $this->getSubjectAssociatedUserAvatarPath(),
+            ])
+            ->add('id', null, [
+                'label' => 'ID',
+            ])
+            ->add('contactParent', null, [
+                'label' => 'Contact parent',
+            ])
+            ->add('associatedUser.email', null, [
+                'label' => 'Email',
+            ])
+            ->add('associatedUser.firstName', null, [
+                'label' => 'First Name',
+            ])
+            ->add('associatedUser.secondName', null, [
+                'label' => 'Second Name',
+            ])
+            ->add('associatedUser.birthday', null, [
+                'label' => 'Birthday',
+            ])
+            ->add('associatedUser.address', null, [
+                'label' => 'Address',
+            ])
+            ->add('associatedUser.phoneNumber', null, [
+                'label' => 'Phone Number',
+            ])
+            ->add('associatedUser.gender', null, [
+                'label' => 'Gender',
+            ])
+            ->end()
+            ->with('Classroom')
+            ->add('classRoom', null, [
+                'label' => 'classroom',
+                'associated_property' => 'uuid',
+            ])
+            ->end();
+    }
+
+    public function getSubjectAssociatedUserAvatarPath(): ?string
+    {
+        $user = $this->getSubject()->getAssociatedUser();
+
+        return $user ? $user->getAvatarPath() : null;
     }
 }
